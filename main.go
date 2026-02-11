7c69ed7197304a36af43eaac3c09c40fbf697b22
Shakil60 <khaldishakil@gmail.com>
2026-02-11 15:49:15 +0100

    fix: import templates

--- main.go
+++ main.go
@@ -5,15 +5,25 @@ import (
 	"net/http"
 	"yboost-portfolio/src/helper"
 	"yboost-portfolio/src/routes"
+	"os"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 func main() {
+	// Chargement des variables d'environnement
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erreur lors du chargement des variables d'environnement: %v", err)
+	}
 	// Chargement des templates
 	helper.Load()
 	// Chargement des routes du serveur
 	serveRouter := routes.MainRouter()
+
+	port := os.Getenv("PORT")
 	// Message d'information indiquant que le serveur est lancé
-	fmt.Println("Server started at http://localhost:8080")
+	fmt.Println("Server started")
 	// Lancement du serveur HTTP sur le port 8081
-	http.ListenAndServe("localhost:8080", serveRouter)
+	http.ListenAndServe(":"+ port, serveRouter)
 }
